a6ba54ca411c8c525128db358dc05739cc80809d
Rutuja Ajagekar <rutuajagekar99@gmail.com>
2020-11-09 16:02:00 +0530

    Create DataTypes.sol

---
+++ Solidity/DataTypes.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.10;
+
+contract DataTypes {
+    
+    bool cok = true;
+    
+    uint8 ksh = 1;
+    uint256 ksh1 = 456;
+    uint ksh2 = 123;
+    
+    
+    int8 i8 = -1;
+    int256 i256 = 456;
+    int i = -123;
+    
+    address add = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    
+    bool defaultBoo; // false
+    uint defaultUint; // 0
+    int defaultInt; // 0
+    address defaultAddr; // 0x0000000000000000000000000000000000000000
+
+
+}
